8148ebec586d23501bbab014fa90959e08f5c365
Zack Waterman <zack.waterman@watermancs.com>
2024-05-16 22:42:22 -0700

    possibly done

--- main.c
+++ main.c
@@ -17,13 +17,14 @@ void initialize_grid() {
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
             grid[i][j].is_alive = 0;
+            grid[i][j].live_neighbors = 0;
         }
     }
 
     grid[5][5].is_alive = 1;
 }
 
-int count_live_neighbors(int x, int y) {
+void count_live_neighbors(int x, int y) {
     int live_count = 0;
 
     int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
@@ -33,33 +34,71 @@ int count_live_neighbors(int x, int y) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if (grid[nx][ny].is_alive) {
-            live_count++;
+        if (nx >= 0 && nx < WIDTH && ny >= 0 && ny < HEIGHT) {
+            if (grid[nx][ny].is_alive) {
+                live_count++;
+            }
         }
     }
 
-    return live_count;
+    grid[x][y].live_neighbors = live_count;
 }
 
 void update_grid() {
+    Cell temp_grid[WIDTH][HEIGHT];
+
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
+            count_live_neighbors(i, j);
+
+            if (grid[i][j].is_alive) {
+                if (grid[i][j].live_neighbors < 2 || grid[i][j].live_neighbors > 3) {
+                    temp_grid[i][j].is_alive = 0;
+                } else {
+                    temp_grid[i][j].is_alive = 1;
+                }
+            } else {
+                if (grid[i][j].live_neighbors == 3) {
+                    temp_grid[i][j].is_alive = 1;
+                } else {
+                    temp_grid[i][j].is_alive = 0;
+                }
+            }
         }
     }
 
-    // Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-    // Any live cell with two or three live neighbors lives on to the next generation.
-    // Any live cell with more than three live neighbors dies, as if by overpopulation.
-    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    for (int i = 0; i < WIDTH; i++) {
+        for (int j = 0; j < HEIGHT; j++) {
+            grid[i][j] = temp_grid[i][j];
+        }
+    }
 }
 
 void display_grid() {
     printf("\033[2J"); // Clear the screen
     printf("\033[H");  // Move cursor to top-left corner
     
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            if (grid[x][y].is_alive) {
+                printf("%c", alive_character);
+            } else {
+                printf("%c", dead_character);
+            }
+        }
 
+        printf("\n");
+    }
 }
 
 int main() {
+    initialize_grid();
 
+    while (1) {
+        display_grid();
+
+        update_grid();
+
+        getchar();
+    }
 }
